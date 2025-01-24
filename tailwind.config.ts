a808c00b1b64c1dbd4af275cd26723eff79eae0f
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2025-01-24 19:32:29 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {},
+		fontFamily: {
+			sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+		}
+	},
+
+	plugins: [daisyui, typography, forms, containerQueries],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#F46C38',
+					'primary-content': '#FFFFFF',
+					secondary: '#c5ff41',
+					'secondary-content': '#160016',
+					accent: '#00ffff',
+					'accent-content': '#001616',
+					neutral: '#ffffff',
+					'neutral-content': '#160016',
+					'base-100': '#151312',
+					'base-200': '#110f0e',
+					'base-300': '#0c0b0a',
+					'base-content': '#fff',
+					info: '#0000ff',
+					'info-content': '#c6dbff',
+					success: '#00ff00',
+					'success-content': '#001600',
+					warning: '#00ff00',
+					'warning-content': '#001600',
+					error: '#ff0000',
+					'error-content': '#160000'
+				}
+			}
+		]
+	}
+} satisfies Config;
