9aaa016b1b1ef8af95c59bfc3b4f6c0312b990e1
Deepjyoti Deb <gouravdeb@gmail.com>
2025-01-25 13:19:21 +0530

    add lucide-svelte icons to Header and update layout in +page.svelte

--- src/lib/Components/Header/Header.svelte
+++ src/lib/Components/Header/Header.svelte
@@ -1,13 +1,46 @@
 <script>
+	import { Briefcase, Folder, House, SquarePen } from 'lucide-svelte';
+
 	// You can add any necessary script here
 </script>
 
 <header class="flex justify-center p-8">
-	<div class="flex max-w-sm gap-2 rounded-2xl bg-[#B6B4BD20] p-4">
-		<div>Home</div>
-		<div>Projects</div>
-		<div>Experience</div>
-		<div>Tools</div>
-		<div>Thoughts</div>
+	<div class="flex gap-8 rounded-2xl bg-[#B6B4BD10] px-4 py-1">
+		<a
+			class="tooltip tooltip-bottom transform transition duration-300 ease-in-out hover:scale-110"
+			data-tip="home"
+			href="/"
+		>
+			<div class="p-2">
+				<House />
+			</div>
+		</a>
+		<a
+			class="tooltip tooltip-bottom transform transition duration-300 ease-in-out hover:scale-110"
+			data-tip="home"
+			href="/"
+		>
+			<div class="p-2">
+				<Folder />
+			</div>
+		</a>
+		<a
+			class="tooltip tooltip-bottom transform transition duration-300 ease-in-out hover:scale-110"
+			data-tip="home"
+			href="/"
+		>
+			<div class="p-2">
+				<Briefcase />
+			</div>
+		</a>
+		<a
+			class="tooltip tooltip-bottom transform transition duration-300 ease-in-out hover:scale-110"
+			data-tip="home"
+			href="/"
+		>
+			<div class="p-2">
+				<SquarePen />
+			</div>
+		</a>
 	</div>
 </header>
